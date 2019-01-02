052f26f7e9ed3db848ab97c5cd507430dfc943d8
Manjeet Kumar <manjeetkumar10588@gmail.com>
2019-01-02 20:23:26 +0530

    Initial commit

---
+++ server/tests/server.test.js
@@ -0,0 +1,53 @@
+var expect = require('expect');
+var request = require('supertest');
+
+var {app} = require('./../server');
+var {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+	Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+	it('should create a new todo', (done) => {
+		var text = 'Test todo Text';
+
+			request(app)
+			.post('/todos')
+			.send({text})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.text).toBe(text);
+			})
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+
+				Todo.find().then((todos) => {
+				expect(todos.length).toBe(1);
+				expect(todos[0].text).toBe(text);
+				done();
+			}).catch((e) => done(e));		
+
+		});
+	});
+
+	it('should not create todo with invalid body data', (done) => {
+
+		request(app)
+		.post('/todo')
+		.send({})
+		.expect(404)
+		.end((err, res) => {
+			if(err, res) {
+				return done(err);
+			}
+
+			Todo.find().then((todos) => {
+				expect(todos.length).toBe(0);
+				done();
+			}).catch((e) => done(e))
+		})
+	})
+});
