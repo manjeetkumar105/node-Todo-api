e52904f2243ab35521a604fa79ae037bbc2edf6c
Manjeet Kumar <manjeetkumar10588@gmail.com>
2019-01-02 21:34:42 +0530

    Initial commit

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ var request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: 'first test todo'
+}, {
+	text: 'second test todo'
+}]
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).toBe(text);
 				done();
@@ -51,3 +59,14 @@ describe('POST /todos', () => {
 		})
 	})
 });
+
+describe('GET /todos', (req, res) => {
+	it('should get all todos', (done) => {
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.todos.length).toBe(2);
+		}).end(done);
+	})
+})
